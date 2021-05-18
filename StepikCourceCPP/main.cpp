a32c0b68cd4f1b302601cdaebf616610c198d1de
lalovkos <52522496+lalovkos@users.noreply.github.com>
2021-05-19 00:05:05 +0700

    2.3 program for array rotating

--- StepikCourceCPP/main.cpp
+++ StepikCourceCPP/main.cpp
@@ -36,9 +36,33 @@ void WriteInConsoleFromLast() {
    
 }
 
+void swap(int a[], int pos1, int pos2) {
+    
+    a[pos1] = a[pos1] + a[pos2];
+    a[pos2] = a[pos1] - a[pos2];
+    a[pos1] = a[pos1] - a[pos2];
+
+}
+
+void rotate(int a[], unsigned size, int shift) {
+    
+    for (int step = 0; step < shift; step++) {
+        for (int i = 0; i < size - 1; i++) {
+            swap(a, i, i + 1);
+        }
+    }
+    
+}
+
 int main() {
-   
-    WriteInConsoleFromLast();
+
+    int a[] = {1,2,3,4,5,6,7,8,9};
+    rotate(a,9,4);
+    for (int i = 0; i < 9; i++) {
+
+        std::cout << a[i];
+    
+    }
 
     return 0;
 }
