376005bd0789350ed03b3c56d043a0fc3ea4fedf
lalovkos <52522496+lalovkos@users.noreply.github.com>
2021-05-24 12:51:25 +0700

    2.4 program 3(last)

--- StepikCourceCPP/main.cpp
+++ StepikCourceCPP/main.cpp
@@ -46,16 +46,15 @@ void swap(int a[], int pos1, int pos2) {
 
 void rotate(int a[], unsigned size, int shift) {
     
-    for (int step = 0; step < shift; step++) {
-        for (int i = 0; i < size - 1; i++) {
+    for (unsigned step = 0; step < shift; step++) {
+        for (unsigned i = 0; i < size - 1; i++) {
             swap(a, i, i + 1);
         }
     }
     
 }
 
-unsigned strlen(const char* str)
-{
+unsigned strlen_my(const char* str) {
     unsigned len = 0;
     while (*str++) {
         ++len;
@@ -63,9 +62,65 @@ unsigned strlen(const char* str)
     return len;
 }
 
+void deleteFirstChars(char* str, const int NFirstToDelete) {
+    
+    if (NFirstToDelete == 0) {
+        return;
+    }
+ 
+    unsigned len = strlen_my(str);
+    for (unsigned i = 0; i < len - NFirstToDelete; i++) {
+        std::cout << str << std::endl;
+        std::cout << strlen_my(str) - NFirstToDelete << std::endl;
+        str[i] = str[i + NFirstToDelete];
+        str[i + NFirstToDelete] = '\0';
+    }
+}
+
+int strstr_my(const char* text, const char* pattern) {
+    
+    unsigned patternlength = strlen_my(pattern);
+    if (!patternlength) {
+        return 0;
+    }
+    
+    unsigned foundlenght = 0;
+    unsigned placecount = 0;
+
+    while (*text != '\0') {
+         
+        for (int i = 0; i < patternlength; i++) {
+            
+            if (text[foundlenght] == pattern[i]) {
+                foundlenght++;
+                if (foundlenght == patternlength) {
+                    return placecount;
+                }
+            }
+            else {
+                foundlenght = 0;
+            }
+        }
+        *++text;
+        placecount++;
+
+    }
+    
+    return (int)-1;
+}
+
 int main() {
-    char str[] = "sdasda";
-    std::cout << strlen(str);
+   
+    
+    char str1[15] = "";
+    char str2[] = "ar";
+    
+    //deleteFirstChars(str1, 3);
+    //deleteFirstChars(str2, 1);
+    std::cout << strstr_my(str1, str2) << std::endl;
+    std::cout << str1 << std::endl;
+    std::cout << str2 << std::endl;
+
 
     return 0;
 }
